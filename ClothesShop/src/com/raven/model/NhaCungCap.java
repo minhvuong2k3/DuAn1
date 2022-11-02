91fffb6eee0bd4f314f006a8a012af28f62f4495
D u o n g <96007994+Dyong46@users.noreply.github.com>
2022-11-02 16:03:53 +0700

    model

---
+++ ClothesShop/src/com/raven/model/NhaCungCap.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.raven.model;
+
+/**
+ *
+ * @author duong
+ */
+public class NhaCungCap {
+    private String maNCC;
+    private String tenNCC;
+    private String sdt;
+    private String email;
+    private String diachi;
+
+    public String getMaNCC() {
+        return maNCC;
+    }
+
+    public void setMaNCC(String maNCC) {
+        this.maNCC = maNCC;
+    }
+
+    public String getTenNCC() {
+        return tenNCC;
+    }
+
+    public void setTenNCC(String tenNCC) {
+        this.tenNCC = tenNCC;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDiachi() {
+        return diachi;
+    }
+
+    public void setDiachi(String diachi) {
+        this.diachi = diachi;
+    }
+    
+    
+}
