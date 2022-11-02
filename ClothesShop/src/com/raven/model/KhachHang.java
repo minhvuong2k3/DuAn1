bd68cea57b2cd659e9c53996ec2f5fc68b27a45b
D u o n g <96007994+Dyong46@users.noreply.github.com>
2022-11-02 23:29:41 +0700

    Cập nhật Model

---
+++ ClothesShop/src/com/raven/model/KhachHang.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.raven.model;
+
+/**
+ *
+ * @author duong
+ */
+public class KhachHang {
+    private String maKH;
+    private String ten;
+    private String sdt;
+    private String email;
+
+    public String getMaKH() {
+        return maKH;
+    }
+
+    public void setMaKH(String maKH) {
+        this.maKH = maKH;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+}
