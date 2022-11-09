031d03cb976581ed30a93b5eae8ac74001746eb3
luongtuanvy <luongngocgiahy0709@gmail.com>
2022-11-09 16:49:43 +0700

    new

--- ClothesShop/src/com/raven/cell/CellName.java
+++ ClothesShop/src/com/raven/cell/CellName.java
@@ -26,8 +26,6 @@ public class CellName extends TableCustomCell {
         txtFirstName = new javax.swing.JTextField();
         txtLastName = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        image = new com.raven.swing.ImageAvatar();
         cmdSave = new javax.swing.JButton();
 
         jLabel1.setForeground(new java.awt.Color(200, 200, 200));
@@ -35,15 +33,6 @@ public class CellName extends TableCustomCell {
 
         jLabel2.setForeground(new java.awt.Color(200, 200, 200));
         jLabel2.setText("Last Name");
-
-        jLabel3.setForeground(new java.awt.Color(200, 200, 200));
-        jLabel3.setText("Image");
-
-        image.setBorderSize(2);
-        image.setBorderSpace(1);
-        image.setGradientColor1(new java.awt.Color(63, 109, 217));
-        image.setGradientColor2(new java.awt.Color(199, 42, 42));
-        image.setImage(new javax.swing.ImageIcon(getClass().getResource("/com/raven/icon/p1.jpg"))); // NOI18N
 
         cmdSave.setText("Save");
 
@@ -59,13 +48,10 @@ public class CellName extends TableCustomCell {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(txtFirstName))
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addComponent(jLabel2)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(txtLastName, javax.swing.GroupLayout.DEFAULT_SIZE, 248, Short.MAX_VALUE)
-                            .addComponent(image, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(txtLastName, javax.swing.GroupLayout.DEFAULT_SIZE, 163, Short.MAX_VALUE)
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(cmdSave, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(0, 0, Short.MAX_VALUE)))))
@@ -82,11 +68,7 @@ public class CellName extends TableCustomCell {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(txtLastName)
                     .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(image, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(cmdSave)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
@@ -110,7 +92,7 @@ public class CellName extends TableCustomCell {
             ModelName d = (ModelName) o;
             txtFirstName.setText(d.getFirstName());
             txtLastName.setText(d.getLastName());
-            image.setImage(d.getProfile());
+//            image.setImage(d.getProfile());
             pathImage = d.getPath();
         }
     }
@@ -119,7 +101,7 @@ public class CellName extends TableCustomCell {
     public Object getData() {
         String firstName = txtFirstName.getText().trim();
         String lastName = txtLastName.getText().trim();
-        return new ModelName(firstName, lastName, image.getImage(), pathImage);
+        return new ModelName(firstName, lastName, pathImage);
     }
 
     @Override
@@ -138,10 +120,8 @@ public class CellName extends TableCustomCell {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton cmdSave;
-    private com.raven.swing.ImageAvatar image;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JTextField txtFirstName;
     private javax.swing.JTextField txtLastName;
     // End of variables declaration//GEN-END:variables
--- ClothesShop/src/com/raven/form/FormHomeStaff.java
+++ ClothesShop/src/com/raven/form/FormHomeStaff.java
@@ -28,18 +28,8 @@ public class FormHomeStaff extends javax.swing.JPanel {
         table1.addTableCell(new CellAddress(), 3);
         table1.addTableCell(new CellTel(), 4);
         table1.addTableCell(new CellAction(), 5);
-        table1.addRow(new ModelStaff(new ModelName("Ra", "Ven", new ImageIcon(getClass().getResource("/com/raven/icon/profile.jpg")), ""), "Male", 18, "Raven123@gmail.com", "010 000 000"), false);  //  ture is animate row
-        table1.addRow(new ModelStaff(new ModelName("Jon", "Cina", new ImageIcon(getClass().getResource("/com/raven/icon/profile.jpg")), ""), "Female", 30, "Raven123@gmail.com", "100 123 123"), false);  //  ture is animate row
-        table1.addRow(new ModelStaff(new ModelName("Ra", "Ven", new ImageIcon(getClass().getResource("/com/raven/icon/profile.jpg")), ""), "Male", 18, "Raven123@gmail.com", "010 000 000"), false);  //  ture is animate row
-        table1.addRow(new ModelStaff(new ModelName("Jon", "Cina", new ImageIcon(getClass().getResource("/com/raven/icon/profile.jpg")), ""), "Female", 30, "Raven123@gmail.com", "100 123 123"), false);  //  ture is animate row
-        table1.addRow(new ModelStaff(new ModelName("Ra", "Ven", new ImageIcon(getClass().getResource("/com/raven/icon/profile.jpg")), ""), "Male", 18, "Raven123@gmail.com", "010 000 000"), false);  //  ture is animate row
-        table1.addRow(new ModelStaff(new ModelName("Jon", "Cina", new ImageIcon(getClass().getResource("/com/raven/icon/profile.jpg")), ""), "Female", 30, "Raven123@gmail.com", "100 123 123"), false);  //  ture is animate row
-        table1.addRow(new ModelStaff(new ModelName("Ra", "Ven", new ImageIcon(getClass().getResource("/com/raven/icon/profile.jpg")), ""), "Male", 18, "Raven123@gmail.com", "010 000 000"), false);  //  ture is animate row
-        table1.addRow(new ModelStaff(new ModelName("Jon", "Cina", new ImageIcon(getClass().getResource("/com/raven/icon/profile.jpg")), ""), "Female", 30, "Raven123@gmail.com", "100 123 123"), false);  //  ture is animate row
-        table1.addRow(new ModelStaff(new ModelName("Ra", "Ven", new ImageIcon(getClass().getResource("/com/raven/icon/profile.jpg")), ""), "Male", 18, "Raven123@gmail.com", "010 000 000"), false);  //  ture is animate row
-        table1.addRow(new ModelStaff(new ModelName("Jon", "Cina", new ImageIcon(getClass().getResource("/com/raven/icon/profile.jpg")), ""), "Female", 30, "Raven123@gmail.com", "100 123 123"), false);  //  ture is animate row
-        table1.addRow(new ModelStaff(new ModelName("Ra", "Ven", new ImageIcon(getClass().getResource("/com/raven/icon/profile.jpg")), ""), "Male", 18, "Raven123@gmail.com", "010 000 000"), false);  //  ture is animate row
-        table1.addRow(new ModelStaff(new ModelName("Jon", "Cina", new ImageIcon(getClass().getResource("/com/raven/icon/profile.jpg")), ""), "Female", 30, "Raven123@gmail.com", "100 123 123"), false);  //  ture is animate row
+        table1.addRow(new ModelStaff(new ModelName("Ra", "Ven", ""), "Male", 18, "Raven123@gmail.com", "010 000 000"), false);  //  ture is animate row
+        table1.addRow(new ModelStaff(new ModelName("Jon", "Cina", ""), "Female", 30, "Raven123@gmail.com", "100 123 123"), false);  //  ture is animate row
     }
 
     @SuppressWarnings("unchecked")
@@ -49,6 +39,7 @@ public class FormHomeStaff extends javax.swing.JPanel {
         roundPanel2 = new com.raven.swing.RoundPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         table1 = new com.raven.swing.Table();
+        roundPanel1 = new com.raven.swing.RoundPanel();
 
         roundPanel2.setBackground(new java.awt.Color(255, 255, 255));
 
@@ -73,20 +64,35 @@ public class FormHomeStaff extends javax.swing.JPanel {
             table1.getColumnModel().getColumn(5).setMaxWidth(100);
         }
 
+        javax.swing.GroupLayout roundPanel1Layout = new javax.swing.GroupLayout(roundPanel1);
+        roundPanel1.setLayout(roundPanel1Layout);
+        roundPanel1Layout.setHorizontalGroup(
+            roundPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 928, Short.MAX_VALUE)
+        );
+        roundPanel1Layout.setVerticalGroup(
+            roundPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+
         javax.swing.GroupLayout roundPanel2Layout = new javax.swing.GroupLayout(roundPanel2);
         roundPanel2.setLayout(roundPanel2Layout);
         roundPanel2Layout.setHorizontalGroup(
             roundPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(roundPanel2Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 928, Short.MAX_VALUE)
+                .addGroup(roundPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1)
+                    .addComponent(roundPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
         roundPanel2Layout.setVerticalGroup(
             roundPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, roundPanel2Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 484, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(roundPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 416, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
 
@@ -106,6 +112,7 @@ public class FormHomeStaff extends javax.swing.JPanel {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JScrollPane jScrollPane1;
+    private com.raven.swing.RoundPanel roundPanel1;
     private com.raven.swing.RoundPanel roundPanel2;
     private com.raven.swing.Table table1;
     // End of variables declaration//GEN-END:variables
